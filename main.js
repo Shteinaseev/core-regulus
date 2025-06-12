924aaec580e066001cd5c4c4209fba396437a2d5
Shteinaseev <142624493+Shteinaseev@users.noreply.github.com>
2025-06-12 15:55:08 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-const sphere = document.getElementById("sphere")
-const rows = []
-for(i = 0; i < 180; i += 9){
-    rows.push(`<div class="line" style="transform: rotateY(${i}deg);"></div>`)
-}
+// const sphere = document.getElementById("sphere")
+// const rows = []
+// for(i = 0; i < 180; i += 10){
+//     rows.push(`<div class="line" style="transform: rotateY(${i}deg);"></div>`)
+// }
 
-sphere.innerHTML = rows.join("");
+// sphere.innerHTML = rows.join("");
 
 const headerEl = document.querySelector(".header")
 
